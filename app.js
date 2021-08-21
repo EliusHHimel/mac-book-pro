c97d4f3ccb560e0d36a8eba29138785e45a35bdf
Hm Elius Hossain Himel <himelahmed201617@gmail.com>
2021-08-21 14:10:41 +0600

    almost complete

--- app.js
+++ app.js
@@ -6,10 +6,12 @@ function extraMemoryPriceUpdate(memoryPrice) {
 
 document.getElementById('memory-8gb').addEventListener('click', function () {
     extraMemoryPriceUpdate(0);
+    calculateTotal('extra-memory-cost');
 })
 
 document.getElementById('memory-16gb').addEventListener('click', function () {
     extraMemoryPriceUpdate(180)
+    calculateTotal('extra-memory-cost');
 })
 
 // Storage Section
@@ -20,12 +22,15 @@ function extraStoragePriceUpdate(storagePrice) {
 
 document.getElementById('ssd-256gb').addEventListener('click', function () {
     extraStoragePriceUpdate(0);
+    calculateTotal('extra-storage-cost');
 })
 document.getElementById('ssd-512gb').addEventListener('click', function () {
     extraStoragePriceUpdate(100);
+    calculateTotal('extra-storage-cost');
 })
 document.getElementById('ssd-1tb').addEventListener('click', function () {
     extraStoragePriceUpdate(180);
+    calculateTotal('extra-storage-cost');
 })
 
 // Delivery Charge Section
@@ -36,8 +41,33 @@ function deliveryChargeUpdate(charge) {
 
 document.getElementById('delivery-free').addEventListener('click', function () {
     deliveryChargeUpdate(0);
+    calculateTotal('delivery-charge')
 })
 
 document.getElementById('delivery-20usd').addEventListener('click', function () {
     deliveryChargeUpdate(20)
+    calculateTotal('delivery-charge')
+})
+
+// Calculate Total
+function calculateTotal(product) {
+    const element = document.getElementById(product).innerText;
+    const total = document.getElementById('total-price').innerText;
+    const totalPrice = parseFloat(element) + parseFloat(total);
+
+    document.getElementById('total-price').innerText = totalPrice;
+    document.getElementById('total-after-promo').innerText = totalPrice;
+}
+
+// Promocode Apply
+document.getElementById('promo-apply-btn').addEventListener('click', function () {
+    const promoInputField = document.getElementById('promocode-input');
+    const promoCode = promoInputField.value;
+    const previousTotal = document.getElementById('total-after-promo').innerText;
+    const promodiscount = parseFloat(previousTotal) * 20 / 100;
+    if (promoCode == 'stevekaku') {
+        document.getElementById('total-after-promo').innerText = parseFloat(previousTotal) - promodiscount;
+    }
+
+    promoInputField.value = '';
 })
